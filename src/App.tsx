5cdfe5359ed94934b19be6bc95668c7932e273c6
Sri <mail@varsan.dk>
2024-03-20 12:50:10 +0100

    Copywriting

--- src/App.tsx
+++ src/App.tsx
@@ -121,57 +121,57 @@ const App: React.FC = () => {
     { label: 'Brug en skabelon', value: 'Template', price: 500, lordicon: 'https://cdn.lordicon.com/ocqzmzvc.json' },
     { label: 'Custom design', value: 'Custom design', price: 1500, lordicon: 'https://cdn.lordicon.com/piwupaqb.json' },
     { label: 'Jeg har ikke brug for design', value: 'Nej design', price: 0, lordicon: 'https://cdn.lordicon.com/oqrxcabg.json' },
-    { label: 'Jeg ved det ikke', value: 'Idk', price: 350, lordicon: 'https://cdn.lordicon.com/oqrxcabg.json' },
+    { label: 'Jeg ved det ikke', value: 'Ukendt', price: 350, lordicon: 'https://cdn.lordicon.com/oqrxcabg.json' },
   ];
 
   const sizeOptions = [
-    { label: 'Lille: mindre end 3 undersider/50 produkter', value: 'Small', price: 0, lordicon: 'https://cdn.lordicon.com/oqrxcabg.json' },
-    { label: 'Mellem: mindre end 10 undersider/200 produkter', value: 'Medium', price: 750, lordicon: 'https://cdn.lordicon.com/oqrxcabg.json' },
-    { label: 'Stor: mere end 10 sider/200 produkter', value: 'Large', price: 1500, lordicon: 'https://cdn.lordicon.com/oqrxcabg.json' },
-    { label: 'Jeg ved det ikke', value: 'Unknown Size', price: 350, lordicon: 'https://cdn.lordicon.com/rjpgjson.json' },
+    { label: 'Lille: mindre end 3 undersider/50 produkter', value: 'Lille', price: 0, lordicon: 'https://cdn.lordicon.com/oqrxcabg.json' },
+    { label: 'Mellem: mindre end 10 undersider/200 produkter', value: 'Mellem', price: 750, lordicon: 'https://cdn.lordicon.com/oqrxcabg.json' },
+    { label: 'Stor: mere end 10 sider/200 produkter', value: 'Stor', price: 1500, lordicon: 'https://cdn.lordicon.com/oqrxcabg.json' },
+    { label: 'Jeg ved det ikke', value: 'Ukendt', price: 350, lordicon: 'https://cdn.lordicon.com/rjpgjson.json' },
   ];
 
   const paymentOptions = [
     { label: 'Ja', value: 'Ja', price: 500, lordicon: 'https://cdn.lordicon.com/vixogsdv.json' },
     { label: 'Nej', value: 'Nej', price: 0, lordicon: 'https://cdn.lordicon.com/athelfnc.json' },
-    { label: 'Jeg ved det ikke', value: 'Unknown Payment', price: 250, lordicon: 'https://cdn.lordicon.com/athelfnc.json' },
+    { label: 'Jeg ved det ikke', value: 'Ukendt', price: 250, lordicon: 'https://cdn.lordicon.com/athelfnc.json' },
   ];
 
   const integrationOptions = [
     { label: 'Ja', value: 'Ja', price: 2500, lordicon: 'https://cdn.lordicon.com/wvsldibu.json' },
     { label: 'Nej', value: 'Nej', price: 0, lordicon: 'https://cdn.lordicon.com/rmkpgtpt.json' },
-    { label: 'Jeg ved det ikke', value: 'Unknown Integration', price: 250, lordicon: 'https://cdn.lordicon.com/rmkpgtpt.json' },
+    { label: 'Jeg ved det ikke', value: 'Ukendt', price: 250, lordicon: 'https://cdn.lordicon.com/rmkpgtpt.json' },
   ];
 
   const loginOptions = [
     { label: 'Ja', value: 'Ja', price: 500, lordicon: 'https://cdn.lordicon.com/wzwygmng.json' },
     { label: 'Nej', value: 'Nej', price: 0, lordicon: 'https://cdn.lordicon.com/ghhwiltn.json' },
-    { label: 'Jeg ved det ikke', value: 'Unknown Login', price: 250, lordicon: 'https://cdn.lordicon.com/rmkpgtpt.json' },
+    { label: 'Jeg ved det ikke', value: 'Ukendt', price: 250, lordicon: 'https://cdn.lordicon.com/rmkpgtpt.json' },
   ];
 
   const multilingualOptions = [
     { label: 'Ja', value: 'Ja', price: 1000, lordicon: 'https://cdn.lordicon.com/wzwygmng.json' },
     { label: 'Nej', value: 'Nej', price: 0, lordicon: 'https://www.howmuchtocreateawebsite.co.uk/img/web/answer-7-2.png' },
-    { label: 'Jeg ved det ikke', value: 'Unknown Multilingual', price: 250, lordicon: 'https://www.howmuchtocreateawebsite.co.uk/img/web/answer-7-3.png' },
+    { label: 'Jeg ved det ikke', value: 'Ukendt', price: 250, lordicon: 'https://www.howmuchtocreateawebsite.co.uk/img/web/answer-7-3.png' },
   ];
 
   const searchOptions = [
     { label: 'Ja', value: 'Ja', price: 350, lordicon: 'https://www.howmuchtocreateawebsite.co.uk/img/web/answer-8-1.png' },
     { label: 'Nej', value: 'Nej', price: 0, lordicon: 'https://www.howmuchtocreateawebsite.co.uk/img/web/answer-8-2.png' },
-    { label: 'Jeg ved det ikke', value: 'Unknown Search', price: 250, lordicon: 'https://www.howmuchtocreateawebsite.co.uk/img/web/answer-8-3.png' },
+    { label: 'Jeg ved det ikke', value: 'Ukendt', price: 250, lordicon: 'https://www.howmuchtocreateawebsite.co.uk/img/web/answer-8-3.png' },
   ];
 
   const seoOptions = [
     { label: 'Ja', value: 'Ja', price: 100, lordicon: 'https://www.howmuchtocreateawebsite.co.uk/img/web/answer-9-1.png' },
     { label: 'Nej', value: 'Nej', price: 0, lordicon: 'https://www.howmuchtocreateawebsite.co.uk/img/web/answer-9-2.png' },
-    { label: 'Jeg ved det ikke', value: 'Unknown SEO', price: 0, lordicon: 'https://www.howmuchtocreateawebsite.co.uk/img/web/answer-9-3.png' },
+    { label: 'Jeg ved det ikke', value: 'Ukendt', price: 0, lordicon: 'https://www.howmuchtocreateawebsite.co.uk/img/web/answer-9-3.png' },
   ];
 
   const stageOptions = [
-    { label: 'Det er bare en idé', value: 'Idea', price: 350, lordicon: 'https://www.howmuchtocreateawebsite.co.uk/img/web/answer-10-1.png' },
+    { label: 'Det er bare en idé', value: 'Idé', price: 350, lordicon: 'https://www.howmuchtocreateawebsite.co.uk/img/web/answer-10-1.png' },
     { label: 'Jeg har sketches', value: 'Sketches', price: 250, lordicon: 'https://www.howmuchtocreateawebsite.co.uk/img/web/answer-10-2.png' },
-    { label: 'Den er under udvikling', value: 'Development', price: 150, lordicon: 'https://www.howmuchtocreateawebsite.co.uk/img/web/answer-10-3.png' },
-    { label: 'Den er allerede lavet', value: 'Created', price: 0, lordicon: 'https://www.howmuchtocreateawebsite.co.uk/img/web/answer-10-4.png' },
+    { label: 'Den er under udvikling', value: 'Under udvikling', price: 150, lordicon: 'https://www.howmuchtocreateawebsite.co.uk/img/web/answer-10-3.png' },
+    { label: 'Den er allerede lavet', value: 'Allerede bygget', price: 0, lordicon: 'https://www.howmuchtocreateawebsite.co.uk/img/web/answer-10-4.png' },
   ];
 
   const renderCurrentStep = () => {
@@ -185,15 +185,15 @@ const App: React.FC = () => {
       case 'payment':
         return <OptionSelection title="Kommer du til at tage imod betaling på din hjemmeside?" options={paymentOptions} onSelect={handlePaymentSelect} />;
       case 'integration':
-        return <OptionSelection title="Will the website be integrated with another website, app or ERP?" options={integrationOptions} onSelect={handleIntegrationSelect} />;
+        return <OptionSelection title="Kommer siden til at blive integreret med en anden hjemmeside, app eller ERP?" options={integrationOptions} onSelect={handleIntegrationSelect} />;
       case 'login':
         return <OptionSelection title="Har du brug for et brugerlogin?" options={loginOptions} onSelect={handleLoginSelect} />;
       case 'multilingual':
         return <OptionSelection title="Skal hjemmesiden kunne bruges på flere sprog?" options={multilingualOptions} onSelect={handleMultilingualSelect} />;
       case 'search':
-        return <OptionSelection title="Does the website need a directory/type of internal search?" options={searchOptions} onSelect={handleSearchSelect} />;
+        return <OptionSelection title="Skal der være en form for intern søgning på siden?" options={searchOptions} onSelect={handleSearchSelect} />;
       case 'seo':
-        return <OptionSelection title="Har du brug for SEO tjenester?" options={seoOptions} onSelect={handleSEOSelect} />;
+        return <OptionSelection title="Har du brug for at vi laver din SEO?" options={seoOptions} onSelect={handleSEOSelect} />;
       case 'stage':
         return <OptionSelection title="Hvilket stadie er din hjemmeside på?" options={stageOptions} onSelect={handleStageSelect} />;
       case 'summary':
--- src/Summary.tsx
+++ src/Summary.tsx
@@ -20,7 +20,7 @@ interface SummaryProps {
 const Summary: React.FC<SummaryProps> = ({ selection, total, onReset }) => {
     return (
         <div>
-            <h1 className="font-bold mb-24 mt-4 text-4xl ">Total estimeret pris: {total} kr</h1>
+            <h1 className="font-bold mb-24 text-4xl ">Total estimeret pris: {total} kr</h1>
             <h2 className="text-2xl font-bold mb-8">Summary</h2>
             <p className="mb-2">Type af hjemmeside: {selection.type}</p>
             <p className="mb-2">Design preference: {selection.design}</p>
@@ -32,7 +32,7 @@ const Summary: React.FC<SummaryProps> = ({ selection, total, onReset }) => {
             <p className="mb-2">Internal search required: {selection.search}</p>
             <p className="mb-2">Brug for SEO services: {selection.seo}</p>
             <p className="mb-2">Hjemmeside stadie: {selection.stage}</p>
-            <button className="p-2 mt-10 mb-10 w-44 rounded-full shadow-lg bg-blue-500 text-white hover:bg-blue-600" onClick={onReset}>Genstart</button>
+            <button className="p-2 mt-10 mb-10 w-44 rounded-full shadow-lg bg-blue-500 text-white hover:bg-blue-600" onClick={onReset}>Prøv igen</button>
         </div>
     );
 };
